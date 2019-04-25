355602a3311412c690ab3ef92a938d7f7f8e736b
Makohan <m.kato.0102@gmail.com>
2019-04-25 23:31:47 +0900

    サークルボタン作成

--- src/elements/CircleButton.js
+++ src/elements/CircleButton.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
-
+import Font from 'expo';
+import fontAwsome from '../../assets/fonts/fa-solid-900.ttf';
 class CircleButton extends React.Component {
+  state = {
+    fontLoaded: false,
+  }
+
+  async componentWillMount() {
+    await Font.loadAsync({
+      FontAwsome: fontAwsome,
+    })
+
+    this.setState({ fontLoaded: true});
+  }
+
   render() {
     const { style, color } = this.props;
 
@@ -15,9 +28,13 @@ class CircleButton extends React.Component {
 
     return (
       <View style={[styles.circleButton, style, { backgroundColor: bgColor }]}>
-        <Text style={[styles.circleButtonTitle, { color: textColor }]}>
-          {this.props.children}
-        </Text>
+        {
+          this.state.setState ? (
+            <Text style={[styles.circleButtonTitle, { color: textColor }]}>
+            {this.props.children}
+          </Text>
+          ) : null
+        }
       </View>
     );
   }
@@ -41,6 +58,7 @@ const styles = StyleSheet.create({
     shadowRadius: 3,
   },
   CirclueButtonTitle: {
+    fontFamily: 'FontAwsome',
     fontSize: 32,
     lineHeight: 32,
     color: '#fff',
