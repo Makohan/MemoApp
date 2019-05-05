b1d467ee968c8f3ed9c191a2a985f784e96a83cc
Makohan <m.kato.0102@gmail.com>
2019-05-05 23:07:22 +0900

    リアルタイムでメモの状態を変更する

--- src/screens/MemoEditScreen.js
+++ src/screens/MemoEditScreen.js
@@ -1,14 +1,65 @@
 import React from 'react';
 import { StyleSheet, View, TextInput } from 'react-native';
 import CircleButton from '../elements/CircleButton';
+
 import firebase from 'firebase';
 
 class MemoEditScreen extends React.Component {  
+  state = {
+    body: '',
+    key: '',
+  }
+
+  componentWillMount() {
+    const { params } = this.props.navigation.state;
+    this.setState({
+      body: params.body,
+      key: params.key,
+    });
+  }
+
+  handlePress() {
+    const { currentUser } = firebase.auth();
+    console.log('currentUser');
+    console.log(currentUser);
+    const newDate = firebase.firestore.Timestamp.now();
+    firebase.firestore().collection(`users/${currentUser.uid}/memos`).doc(this.state.key)
+      .update({
+        body: this.state.body,
+        createdOn: newDate,
+      })
+      .then(() => {
+        const { navigation } = this.props;
+        navigation.state.params.returnMemo({
+          body: this.state.body,
+          key: this.state.key,
+          createdOn: newDate,
+        })
+        navigation.goBack();
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
   render() {
     return(
       <View style={styles.container}>
-        <TextInput style={styles.memoEditInput} multiline value='Hi' />
-        <CircleButton name='check' onPress={() => {this.props.navigation.goBack(); }} />
+        <TextInput
+          style={styles.memoEditInput}
+          multiline
+          value={this.state.body}
+          onChangeText={(text) => {
+            this.setState({
+              body: text,
+              createdOn: new Date(),
+            }); 
+          }}
+        />
+        <CircleButton 
+          name='check'
+          onPress={this.handlePress.bind(this)}
+        />
       </View>
     );
   }
